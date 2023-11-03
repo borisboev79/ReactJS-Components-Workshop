839abaac0b246e301963490404a05e7ba4b90ef7
Boris Boev <boris.boev@gmail.com>
2023-11-04 00:13:33 +0200

    <first commit>

---
+++ src/components/UserList.jsx
@@ -0,0 +1,12 @@
+import SearchBar from "./SearchBar";
+import UserListTable from "./UserListTable";
+
+const UserList = () => {
+  return <section className="card users-container">
+   <SearchBar />
+   <UserListTable />
+
+  </section>;
+};
+
+export default UserList;
